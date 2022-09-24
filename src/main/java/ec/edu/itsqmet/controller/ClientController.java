bbf691290d97e5694a4e93ac00fd61473fd0e339
Leonardo Santo <leonard_sc@hotmail.com>
2022-09-24 05:22:29 -0500

    Tarea para Evaluación

--- src/main/java/ec/edu/itsqmet/controller/ClientController.java
+++ src/main/java/ec/edu/itsqmet/controller/ClientController.java
@@ -1,16 +1,39 @@
 package ec.edu.itsqmet.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ec.edu.itsqmet.service.IClientService;
 
 @RestController
 @RequestMapping("/client")
 public class ClientController {
-	
-	@GetMapping("/saludar")	
-	public String saludar() {
-		return "Hola Mundo";
 
+	@Autowired
+	private IClientService clientService;
+
+	@GetMapping("/saludarGet")
+	public String saludarGet() {
+		return clientService.saludarGet();
+	}
+
+	@PostMapping("/saludarPost")
+	public String saludarPost() {
+		return clientService.saludarPost();
+	}
+
+	@PutMapping("/saludarPut")
+	public String saludarPut() {
+		return clientService.saludarPut();
+	}
+
+	@DeleteMapping("/saludarDelete")
+	public String saludarDeletet() {
+		return clientService.saludarDelete();
 	}
 }
